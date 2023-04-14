d3bd5623d47ed5fed8f9bcf7944c79aa6cb22b3b
Nikhil Badole <nikhilbadole4@gmail.com>
2023-04-15 01:28:35 +0530

    First

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
 public class Main {
+    public static class Product{
+        public int product(int x, int y) {
+            return 0;
+        }
 
+        //@Override
+        public int product(int x, int y, int z) {
+            return 1;
+        }
+
+        //@Override
+        public double product(double x, double y) {
+            return 1.2;
+        }
+    }
+
+    public static void main(String[] args){
+        Product p = new Product();
+        p.product(0,1);
+        p.product(0,1,2);
+        p.product(1.1,1.2);
+    }
 }
